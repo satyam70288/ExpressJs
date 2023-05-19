7986ed31b72eb3399a70424b3da9f34daa675c75
robowood <satyamb971@gmail.com>
2023-05-19 17:49:53 +0530

    middleware

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const express=require('express');
 
 // console.log(routes.someText);
 const app=express();
+app.use((req,res,next)=>{
+    console.log('middleware')
+    next();
+});
+app.use((req,res,next)=>{
+    console.log(' another middleware')
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
